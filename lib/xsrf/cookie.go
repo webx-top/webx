27e3ebca7e23623f9c5a1b500811a61da413fc91
swh <swhbox@foxmail.com>
2016-01-27 16:31:48 +0800

    update

--- lib/xsrf/cookie.go
+++ lib/xsrf/cookie.go
@@ -5,6 +5,22 @@ import (
 	X "github.com/webx-top/webx"
 )
 
+type SecCookieStorage struct {
+}
+
+func (c *SecCookieStorage) Get(key string, ctx echo.Context) string {
+	val, _ := X.X(ctx).GetSecCookie(key).(string)
+	return val
+}
+
+func (c *SecCookieStorage) Set(key, val string, ctx echo.Context) {
+	X.X(ctx).SetSecCookie(key, val)
+}
+
+func (c *SecCookieStorage) Valid(key, val string, ctx echo.Context) bool {
+	return c.Get(key, ctx) == val
+}
+
 type CookieStorage struct {
 }
 
